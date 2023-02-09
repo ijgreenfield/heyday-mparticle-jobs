2c15396354df6c308b4e79f2dfab2c52d37b9472
ianjgreenfield Greenfield <104524882+ijgreenfield@users.noreply.github.com>
2023-02-09 22:55:22 +0000

    asknicely webhook test

--- netlify/functions/mparticle-jobs/mparticle-jobs.js
+++ netlify/functions/mparticle-jobs/mparticle-jobs.js
@@ -1,14 +1,8 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
+const handler = async (event, context) => {
+  const body = JSON.parse(event.body);
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    console.log(body.person.name)
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
