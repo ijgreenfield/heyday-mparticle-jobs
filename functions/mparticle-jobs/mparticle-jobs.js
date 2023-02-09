ebf639d1e23b5d4b617e8d06d02440be87190a44
ianjgreenfield Greenfield <104524882+ijgreenfield@users.noreply.github.com>
2023-02-09 23:00:54 +0000

    retry

--- functions/mparticle-jobs/mparticle-jobs.js
+++ functions/mparticle-jobs/mparticle-jobs.js
@@ -1,11 +1,4 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event, context) => {
-  const body = JSON.parse(event.body);
-  try {
-    console.log(body.person.name)
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+exports.handler = async function(event, context) {
+  const body = JSON.parse(event.body)
+  console.log(body.person.name)
 }
-
-module.exports = { handler }
